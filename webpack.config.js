fd0f5f2ce60e01b4ab9965b05a1296deee34292a
viv231912 <schen231912@gmail.com>
2024-08-15 13:47:01 +1000

    project setup

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+
+module.exports = {
+    //mode: 'development',
+    entry: './src/index.js',
+    devServer: {
+        static:'./dist'
+    },
+   plugins: [
+     new HtmlWebpackPlugin({
+       title: 'Development',
+     }),
+    ],  
+    output: {
+        path: path.resolve(__dirname, 'dist/assets'),
+        filename: 'bundle.js'
+    },
+   module:{
+    rules:[
+        {
+            test: /\.js$/,
+            exclude:/node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options:{
+                    presets:[ 
+                        ['@babel/preset-env']
+                    ]
+                }
+            }
+        },
+        {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'] 
+        }
+    ]
+   }
+}
