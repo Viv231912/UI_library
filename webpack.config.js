e46d823ccdcd130a0d4c182f0ec2d86b07c705fd
viv231912 <schen231912@gmail.com>
2024-08-16 13:20:39 +1000

    update webpack config with plugin

--- webpack.config.js
+++ webpack.config.js
@@ -6,19 +6,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     //mode: 'development',
     entry: './src/index.js',
-    devServer: {
-        static:'./dist'
-    },
-//    plugins: [
-//      new HtmlWebpackPlugin({
-//        title: 'Development',
-//        template:'./dist/assets/index.html',
-//        filename:'index.html'
-//      }),
-//     ],  
+   
+   plugins: [
+     new HtmlWebpackPlugin({
+       title: 'Development',
+       template:'./dist/index.html',
+       filename:'index.html'
+     }),
+    ],  
     output: {
         path: path.resolve(__dirname, 'dist/assets'),
         filename: 'bundle.js'
+    },
+    devServer: {
+        static:  path.resolve(__dirname, 'dist'),
+           
+        
+        
     },
    module:{
     rules:[
